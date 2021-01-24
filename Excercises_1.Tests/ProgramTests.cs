675cefeb744e5b4bf64f0de452b51070d7b91545
Francisco Nieves <francisconi@herbalife.com>
2021-01-24 13:41:09 -0600

    Add tests and templates

---
+++ Excercises_1.Tests/ProgramTests.cs
@@ -0,0 +1,268 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Excercises_1.Tests
+{
+    [TestClass]
+    public class ProgramTests
+    {
+        #region CountDs
+        //[TestMethod]
+        //public void CountDs_Dylan()
+        //{
+        //    var result = Program.CountDs("My friend Dylan got distracted at school");
+
+        //    Assert.AreEqual(4, result);
+        //}
+
+        //[TestMethod]
+        //public void CountDs_Debris()
+        //{
+        //    var result = Program.CountDs("Debris was scattered all over the place.");
+
+        //    Assert.AreEqual(2, result);
+        //}
+
+        //[TestMethod]
+        //public void CountDs_Rodents()
+        //{
+        //    var result = Program.CountDs("The rodents hibernated in their den.");
+
+        //    Assert.AreEqual(3, result);
+        //}
+        #endregion
+
+        #region SmallerNum
+        //[TestMethod]
+        //public void SmallerNum_21_44()
+        //{
+        //    var result = Program.SmallerNum("21", "44");
+
+        //    Assert.AreEqual("21", result);
+        //}
+
+        //[TestMethod]
+        //public void SmallerNum_153_367()
+        //{
+        //    var result = Program.SmallerNum("153", "367");
+
+        //    Assert.AreEqual("153", result);
+        //}
+
+        //[TestMethod]
+        //public void SmallerNum_1500_16()
+        //{
+        //    var result = Program.SmallerNum("1500", "16");
+
+        //    Assert.AreEqual("16", result);
+        //}
+
+        //[TestMethod]
+        //public void SmallerNum_100_23()
+        //{
+        //    var result = Program.SmallerNum("100", "23");
+
+        //    Assert.AreEqual("23", result);
+        //}
+
+        //[TestMethod]
+        //public void SmallerNum_1500_1()
+        //{
+        //    var result = Program.SmallerNum("1500", "1");
+
+        //    Assert.AreEqual("1", result);
+        //}
+
+        //[TestMethod]
+        //public void SmallerNum_5_5()
+        //{
+        //    var result = Program.SmallerNum("5", "5");
+
+        //    Assert.AreEqual("5", result);
+        //}
+        #endregion
+
+        #region Repetition
+        //[TestMethod]
+        //public void Repetition_soccer()
+        //{
+        //    var result = Program.Repetition("soccer", 2);
+
+        //    Assert.AreEqual("soccersoccer", result);
+        //}
+
+        //[TestMethod]
+        //public void Repetition_ab()
+        //{
+        //    var result = Program.Repetition("ab", 3);
+
+        //    Assert.AreEqual("ababab", result);
+        //}
+
+        //[TestMethod]
+        //public void Repetition_this()
+        //{
+        //    var result = Program.Repetition("this", 1);
+
+        //    Assert.AreEqual("this", result);
+        //}
+
+        //[TestMethod]
+        //public void Repetition_ciao()
+        //{
+        //    var result = Program.Repetition("ciao", 4);
+
+        //    Assert.AreEqual("ciaociaociaociao", result);
+        //}
+
+        //[TestMethod]
+        //public void Repetition_charon()
+        //{
+        //    var result = Program.Repetition("charon", 5);
+
+        //    Assert.AreEqual("charoncharoncharoncharoncharon", result);
+        //}
+
+        //[TestMethod]
+        //public void Repetition_sa()
+        //{
+        //    var result = Program.Repetition("sa", 10);
+
+        //    Assert.AreEqual("sasasasasasasasasasa", result);
+        //}
+        #endregion
+
+        #region ReverseCase
+        //[TestMethod]
+        //public void ReverseCase_HappyBirthday()
+        //{
+        //    var result = Program.ReverseCase("Happy Birthday");
+
+        //    Assert.AreEqual("hAPPY bIRTHDAY", result);
+        //}
+
+        //[TestMethod]
+        //public void ReverseCase_Thanks()
+        //{
+        //    var result = Program.ReverseCase("MANY THANKS");
+
+        //    Assert.AreEqual("many thanks", result);
+        //}
+
+        //[TestMethod]
+        //public void ReverseCase_Spontaneous()
+        //{
+        //    var result = Program.ReverseCase("sPoNtAnEoUs");
+
+        //    Assert.AreEqual("SpOnTaNeOuS", result);
+        //}
+
+        //[TestMethod]
+        //public void ReverseCase_Excellent()
+        //{
+        //    var result = Program.ReverseCase("eXCELLENT, yOuR mAJESTY");
+
+        //    Assert.AreEqual("Excellent, YoUr Majesty", result);
+        //}
+        #endregion
+
+        #region ArrayOfMultiples
+        //[TestMethod]
+        //public void ArrayOfMultiples_7_5()
+        //{
+        //    var result = Program.ArrayOfMultiples(7, 5);
+
+        //    CollectionAssert.AreEqual(new int[] { 7, 14, 21, 28, 35 }, result);
+        //}
+
+        //[TestMethod]
+        //public void ArrayOfMultiples_12_10()
+        //{
+        //    var result = Program.ArrayOfMultiples(12, 10);
+
+        //    CollectionAssert.AreEqual(new int[] { 12, 24, 36, 48, 60, 72, 84, 96, 108, 120 }, result);
+        //}
+
+        //[TestMethod]
+        //public void ArrayOfMultiples_17_7()
+        //{
+        //    var result = Program.ArrayOfMultiples(17, 7);
+
+        //    CollectionAssert.AreEqual(new int[] { 17, 34, 51, 68, 85, 102, 119 }, result);
+        //}
+
+        //[TestMethod]
+        //public void ArrayOfMultiples_630_14()
+        //{
+        //    var result = Program.ArrayOfMultiples(630, 14);
+
+        //    CollectionAssert.AreEqual(new int[] { 630, 1260, 1890, 2520, 3150, 3780, 4410, 5040, 5670, 6300, 6930, 7560, 8190, 8820 }, result);
+        //}
+
+        //[TestMethod]
+        //public void ArrayOfMultiples_140_3()
+        //{
+        //    var result = Program.ArrayOfMultiples(140, 3);
+
+        //    CollectionAssert.AreEqual(new int[] { 140, 280, 420 }, result);
+        //}
+
+        //[TestMethod]
+        //public void ArrayOfMultiples_7_8()
+        //{
+        //    var result = Program.ArrayOfMultiples(7, 8);
+
+        //    CollectionAssert.AreEqual(new int[] { 7, 14, 21, 28, 35, 42, 49, 56 }, result);
+        //}
+
+        //[TestMethod]
+        //public void ArrayOfMultiples_11_21()
+        //{
+        //    var result = Program.ArrayOfMultiples(11, 21);
+
+        //    CollectionAssert.AreEqual(new int[] { 11, 22, 33, 44, 55, 66, 77, 88, 99, 110, 121, 132, 143, 154, 165, 176, 187, 198, 209, 220, 231 }, result);
+        //}
+        #endregion
+
+        #region KaracaEncrypt
+        //[TestMethod]
+        //public void Encrypt_karaca()
+        //{
+        //    var result = Program.Encrypt("karaca");
+
+        //    Assert.AreEqual("0c0r0kaca", result);
+        //}
+
+        //[TestMethod]
+        //public void Encrypt_burak()
+        //{
+        //    var result = Program.Encrypt("burak");
+
+        //    Assert.AreEqual("k0r3baca", result);
+        //}
+
+        //[TestMethod]
+        //public void Encrypt_alpaca()
+        //{
+        //    var result = Program.Encrypt("alpaca");
+
+        //    Assert.AreEqual("0c0pl0aca", result);
+        //}
+
+        //[TestMethod]
+        //public void Encrypt_banana()
+        //{
+        //    var result = Program.Encrypt("banana");
+
+        //    Assert.AreEqual("0n0n0baca", result);
+        //}
+
+        //[TestMethod]
+        //public void Encrypt_hello()
+        //{
+        //    var result = Program.Encrypt("hello");
+
+        //    Assert.AreEqual("2ll1haca", result);
+        //}
+        #endregion
+    }
+}
---
+++ Excercises_1.Tests/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Excercises_1.Tests")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Excercises_1.Tests")]
+[assembly: AssemblyCopyright("Copyright ©  2021")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("59c4562d-639d-4d62-9c7b-89418a1c1ea2")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
